dabfc56808e1b0e1e5b3c0e09c499e75b23d3962
Ömer Faruk DEMİR <138582675+demiromerfaruk@users.noreply.github.com>
2023-11-03 07:06:12 +0000

    added docker, dockercompose and start.sh for ci/cd stuff

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ci_cd_test.Controllers;
+
+[ApiController]
+[Route("/test")]
+public class TestController : ControllerBase
+{
+
+    [HttpGet]
+    public string Get()
+    {
+       return "Galatasaray";
+    }
+}
